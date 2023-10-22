804edab3921f39e7039e4a3fbf6f76ba4bfbaa21
ezqu <113508001+ezqu@users.noreply.github.com>
2023-10-23 00:53:24 +0200

    Getting the hitboxes

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ document.addEventListener("DOMContentLoaded", function () {
     let canvas = document.getElementById("canvas");
     let ctx = canvas.getContext("2d");
 
-    let width = 1060;
-    let height = 600;
+    let width = 1024;
+    let height = 576;
 
     canvas.width = width;
     canvas.height = height;
+
+    ctx.fillRect(0, 0, width, height);
+
+    class Sprite {
+        constructor(postion, size, color) {
+            this.postion = postion;
+            this.size = size;
+            this.color = color;
+        }
+
+        draw() {
+            ctx.fillStyle = "red";
+            ctx.fillRect(this.postion.x, this.postion.y,
+                 100, 200);
+
+            }
+    }
+
+    const player = new Sprite({ x: 50, y: 100 },
+         { width: 100, height: 100 }, "red");
+
+    player.draw();
+
+
+    const enemy = new Sprite({ x: 900, y: 100 },
+         { width: 100, height: 100 }, "blue");
+
+         enemy.draw();
+
+
+         console.log(player);
 
 });

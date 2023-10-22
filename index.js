0c2b3b442389164b0d326880c12565ee803acff5
ezqu <113508001+ezqu@users.noreply.github.com>
2023-10-23 00:36:30 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+
+    let width = 1060;
+    let height = 600;
+
+    canvas.width = width;
+    canvas.height = height;
+
+});
